4d0a84067ca0ebdffbd2af41f81a51522c71aaf8
Gaël Pardonnet <gael.pardonnet@hotmail.fr>
2025-06-30 10:13:05 +0200

    add testing

--- services/PlayerService.js
+++ services/PlayerService.js
@@ -13,3 +13,10 @@ module.exports.createPlayer = async (playerData) => {
     const player = new Player(playerData)
     return await player.save()
 }
+
+module.exports.deletePlayer = async (playerId) => {
+   let result = await Player.findByIdAndDelete(playerId)
+   if (!result)
+        throw {type: 'NOT_FOUND', message: 'Impossible de trouver le player a supprimer.'}
+    return result
+}
--- tests/PlayerService.test.js
+++ tests/PlayerService.test.js
@@ -4,17 +4,35 @@ const mongoose = require('mongoose')
 
 main().catch(err => console.log(err));
 
+var users = []
+
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
-    createPlayer()
+    await createPlayer({})
+    await createPlayer({username: "Louis"})
+        await createPlayer([{username: "Louis"}])
+    await deletePlayer("Edoaurd")
+    await deletePlayer("68623fa5d7eff9810f11602f")
+    await deletePlayer(users[0]?._id)
 }
 
-async function createPlayer() {
+async function createPlayer(body) {
     try {
-        await PlayerService.createPlayer({})
+        let res = await PlayerService.createPlayer(body)
+        users.push(res)
         console.log('Player ajouté')
     }
     catch (err) {
         console.log('Player non ajouté, probleme :', err?.message)
+    }
+}
+
+async function deletePlayer(id) {
+      try {
+        let res = await PlayerService.deletePlayer(id)
+        console.log('Player supprimer')
+    }
+    catch (err) {
+        console.log('Player non supprimer, probleme :', err?.message)
     }
 }
