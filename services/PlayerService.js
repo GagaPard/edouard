aa6062ed32541fc4f4ce83c55410aed335270fd3
Gaël Pardonnet <gael.pardonnet@hotmail.fr>
2025-06-30 15:46:57 +0200

    add testing

--- services/PlayerService.js
+++ services/PlayerService.js
@@ -9,14 +9,64 @@ module.exports.getAllPlayers = async () => {
     return await Player.find()
 }
 
+module.exports.getPlayer = async (playerId) => {
+    return await Player.findById(playerId)
+}
+
+module.exports.getPlayersByFilter = async (filter) => {
+    return await Player.find(filter)
+}
+
 module.exports.createPlayer = async (playerData) => {
     const player = new Player(playerData)
     return await player.save()
 }
 
+module.exports.createPlayers = async(playersData) => {
+    for (const data of playersData) {
+        const player = new Player(data)
+        await player.validate()     //valide le schéma comme runValidators en gros
+    }
+    return await Player.insertMany(playersData, { ordered: true }) //ordered : true fait s'arrêter le process à la moindre erreur
+}
+
 module.exports.deletePlayer = async (playerId) => {
-   let result = await Player.findByIdAndDelete(playerId)
-   if (!result)
-        throw {type: 'NOT_FOUND', message: 'Impossible de trouver le player a supprimer.'}
+    let result = await Player.findByIdAndDelete(playerId)
+    if (!result)
+        throw {type: 'NOT_FOUND', message: 'Impossible de trouver le player à supprimer.'}
     return result
 }
+
+module.exports.deletePlayersByFilter = async (filter) => {
+    const result = await Player.deleteMany(filter)
+
+    if (!result.acknowledged)
+        throw { type: 'DB_ERROR', message: 'Requête impossible.' }
+
+    if (result.deletedCount === 0)
+        throw { type: 'NOT_FOUND', message: 'Aucun player trouvé à supprimer.' }
+
+    return result
+}
+
+module.exports.modifyPlayer= async (playerId, updatedData) => {
+    const result = await Player.findByIdAndUpdate(playerId, updatedData, {
+        new: true,              //Retourne le document après modification
+        runValidators: true     //Oblige à respeceter les règles imposées dans le schéma (exemple : le type)
+    })
+    if (!result)
+        throw {type: 'NOT_FOUND', message: 'Impossible de trouver le player à supprimer.'}
+    return result
+}
+
+module.exports.modifyPlayersByFilter = async (filter, updateData) => {
+    const playersToUpdate = await Player.find(filter).select('_id')
+    const ids = playersToUpdate.map(p => p._id)
+
+    if (ids.length === 0)
+        throw {type: 'NOT_FOUND', message: 'Aucun player trouvé à supprimer.'}
+
+    await Player.updateMany({ _id: { $in: ids } }, updateData, { runValidators: true })
+    return Player.find({ _id: { $in: ids } })
+}
+
--- tests/PlayerService.test.js
+++ tests/PlayerService.test.js
@@ -7,12 +7,19 @@ main().catch(err => console.log(err));
 var users = []
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
-    await createPlayer({})
-    await createPlayer({username: "Louis"})
-        await createPlayer([{username: "Louis"}])
-    await deletePlayer("Edoaurd")
-    await deletePlayer("68623fa5d7eff9810f11602f")
+    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    await createPlayer({username: "Louis", score : 300})
+    await createPlayers([{username : "Pablo"}, {username : "La petite soeur de Pablo"}, {username: "Edouard dans la piscine creusée", score: 100}])
+    await modifyPlayer(users[0]?._id, {username: "Ryad"})
+    await getPlayer(users[0]?._id)
+    await getPlayersByFilter({score: 0})
+    await getPlayersByIds([users[0]?._id, users[1]?._id])
+    await modifyPlayersByIds([users[1]?._id, users[2]?._id], {score: 100 })
+    await modifyPlayer(users[1]?._id, {score: 0})
+    await modifyPlayersByFilter({score: 0 }, {score: 100 })
+    await getPlayersByFilter({score: 100})
+    await deletePlayersByIds([users[1]?._id, users[2]?._id])
+    await deletePlayersByFilter({score : 100})
     await deletePlayer(users[0]?._id)
 }
 
@@ -23,16 +30,171 @@ async function createPlayer(body) {
         console.log('Player ajouté')
     }
     catch (err) {
-        console.log('Player non ajouté, probleme :', err?.message)
+        console.log('Player non ajouté, problème :', err?.message)
+    }
+}
+
+async function createPlayers(listOfPlayers) {
+    try {
+        let res = await PlayerService.createPlayers(listOfPlayers)
+        users.push(...res)
+        console.log('Players ajoutés !')
+    }
+    catch (err) {
+        console.log('Players non ajoutés : problème :', err?.message)
     }
 }
 
 async function deletePlayer(id) {
-      try {
+    try {
         let res = await PlayerService.deletePlayer(id)
-        console.log('Player supprimer')
+        console.log('Player supprimé')
     }
     catch (err) {
-        console.log('Player non supprimer, probleme :', err?.message)
+        console.log('Player non supprimé, problème :', err?.message)
+    }
+}
+
+async function deletePlayersByFilter(filter) {
+    try {
+        const result = await PlayerService.deletePlayersByFilter(filter)
+        console.log(`${result.deletedCount} player(s) supprimé(s) avec succès !`)
+    } catch (err) {
+        console.log('Players non supprimés :', err?.message)
+    }
+}
+
+async function deletePlayersByIds(ids) {
+    try {
+        const validIds = ids.filter(Boolean)
+
+        if (validIds.length === 0) {
+            console.log('Aucun ID rentré.')
+            return
+        }
+
+        const result = await PlayerService.deletePlayersByFilter({ _id: { $in: validIds } })
+
+        console.log(`${result.deletedCount} joueur(s) supprimé(s) avec succès.`)
+    } catch (err) {
+        console.log('Erreur lors de la suppression :', err?.message)
+    }
+}
+
+async function modifyPlayer(id, update) {
+    try {
+        let res = await PlayerService.modifyPlayer(id, update)
+        console.log(`Player modifié : ${res.username} avec un score de ${res.score}`)
+    }
+    catch (err) {
+        console.log('Player non modifié, problème:', err?.message)
+    }
+}
+
+async function modifyPlayersByFilter(filter, updateData) {
+    try {
+        const updatedPlayers = await PlayerService.modifyPlayersByFilter(filter, updateData)
+
+        if (updatedPlayers.length === 0) {
+            console.log('Aucun player modifié avec ce filtre.')
+            return []
+        }
+
+        console.log(`${updatedPlayers.length} player(s) modifié(s) avec succès :`)
+        updatedPlayers.forEach(player => {
+            console.log(`- ${player.username} avec un score de ${player.score}`)
+        })
+
+        return updatedPlayers
+
+    } catch (err) {
+        console.log('Erreur lors de la modification des players :', err?.message)
+        return []
+    }
+}
+
+async function modifyPlayersByIds(ids, updateData) {
+    try {
+        const validIds = ids.filter(Boolean)
+
+        if (validIds.length === 0) {
+            console.log('Aucun ID fourni.')
+            return []
+        }
+
+        const updatedPlayers = await PlayerService.modifyPlayersByFilter({ _id: { $in: validIds } }, updateData)
+
+        if (updatedPlayers.length === 0) {
+            console.log('Aucun player modifié pour ces IDs.')
+            return []
+        }
+
+        console.log(`${updatedPlayers.length} player(s) modifié(s) avec succès :`)
+        updatedPlayers.forEach(player => {
+            console.log(`- ${player.username} avec un score de ${player.score}`)
+        })
+        return updatedPlayers
+
+    } catch (err) {
+        console.log('Erreur lors de la modification des players :', err?.message)
+        return []
+    }
+}
+
+async function getPlayer(id) {
+    try {
+        let res = await PlayerService.getPlayer(id)
+        console.log(`Le joueur que tu recherches : ${res.username} avec un score de ${res.score}`)
+    }
+    catch (err) {
+        console.log('Player non trouvé, problème:', err?.message)
+    }
+}
+
+async function getPlayersByFilter(filter) {
+    try {
+        const players = await PlayerService.getPlayersByFilter(filter)
+
+        if (!players.length) {
+            console.log('Aucun player trouvé avec ce filtre.')
+            return []
+        }
+
+        console.log(`${players.length} player(s) trouvé(s) avec succès :`)
+        players.forEach(player => {
+            console.log(`- ${player.username} avec un score de ${player.score}`);
+        })
+        return players
+    } catch (err) {
+        console.log('Erreur lors de la récupération des players :', err?.message)
+        return []
+    }
+}
+
+async function getPlayersByIds(ids) {
+    try {
+        const validIds = ids.filter(Boolean)
+
+        if (validIds.length === 0) {
+            console.log('Aucun ID rentré.')
+            return []
+        }
+
+        const players = await PlayerService.getPlayersByFilter({ _id: { $in: validIds } });
+
+        if (!players.length) {
+            console.log('Aucun player trouvé pour ces IDs.')
+            return []
+        }
+
+        console.log(`${players.length} player(s) trouvé(s) avec succès :`)
+        players.forEach(player => {
+            console.log(`- ${player.username} avec un score de ${player.score}`)
+        })
+        return players
+
+    } catch (err) {
+        console.log('Erreur lors de la récupération des players :', err?.message)
+        return []
     }
 }
